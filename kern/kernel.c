9823270eee20b8ea214b98a46b055beb8cbe649d
tding <tding@andrew.cmu.edu>
2014-10-16 13:44:24 -0400

    Stuck on the bug during switch from kernel mode to user mode

--- kern/kernel.c
+++ kern/kernel.c
@@ -14,6 +14,7 @@
 #include <common_kern.h>
 #include "console.h"
  #include <malloc.h>
+ #include "eflags.h"
 /* libc includes. */
 #include <stdio.h>
 #include <simics.h>                 /* lprintf() */
@@ -24,13 +25,15 @@
 /* x86 specific includes */
 #include <x86/asm.h>                /* enable_interrupts() */
 extern int handler_install(void (*tickback)(unsigned int));
-extern void set_ss(int i);
+extern void set_ss();
 extern void set_ds(int i);
 extern void set_es(int i);
 extern void set_cs(int i);
 extern void set_esp(int i);
 extern KF* mm_init();
+void allocate_page(uint32_t virtual_addr, size_t size);
 static void tick(unsigned int numTicks);
+
 uint64_t seconds;
 
 static KF *frame_base = 0;
@@ -47,7 +50,7 @@ int kernel_main(mbinfo_t *mbinfo, int argc, char **argv, char **envp)
      * You should delete this comment, and enable them --
      * when you are ready.
      */
-handler_install(tick);
+    handler_install(tick);
     lprintf( "Hello from a brand new kernel! %lu",get_esp0());
     
     //clear_console();
@@ -59,7 +62,7 @@ handler_install(tick);
     //set_ss(213123);
 
     // MAGIC_BREAK;
-frame_base = mm_init();
+    frame_base = mm_init();
 
 
     // f = (void *)(se_hdr.e_entry);
@@ -88,14 +91,41 @@ frame_base = mm_init();
     lprintf("e_txtoff: %lu",se_hdr.e_txtoff);
     lprintf("e_txtlen: %lu",se_hdr.e_txtlen);
 
+
+        lprintf("e_datstart: %lx",se_hdr.e_datstart);
+    lprintf("e_datoff: %lu",se_hdr.e_datoff);
+    lprintf("e_datlen: %lu",se_hdr.e_datlen);
+
+
+        lprintf("e_rodatstart: %lx",se_hdr.e_rodatstart);
+    lprintf("e_rodatoff: %lu",se_hdr.e_rodatoff);
+    lprintf("e_rodatlen: %lu",se_hdr.e_rodatlen);
+
+
+        lprintf("e_bssstart: %lx",se_hdr.e_bssstart);
+    lprintf("e_bsslen: %lu",se_hdr.e_bsslen);
+
+
+// MAGIC_BREAK;
 lprintf("%p", frame_base);
+allocate_page((uint32_t)se_hdr.e_datstart, se_hdr.e_datlen);
+allocate_page((uint32_t)se_hdr.e_txtstart,se_hdr.e_txtlen);
+allocate_page((uint32_t)se_hdr.e_rodatstart, se_hdr.e_rodatlen);
+allocate_page((uint32_t)se_hdr.e_bssstart,se_hdr.e_bsslen);
+allocate_page((uint32_t)0xffffefff,4096);
+ // *(int *)0xfffffffc=3;
+/* copy data from data field */
+getbytes(se_hdr.e_fname, se_hdr.e_datoff, se_hdr.e_datlen, (char *)se_hdr.e_datstart);
+getbytes(se_hdr.e_fname, se_hdr.e_txtoff, se_hdr.e_txtlen, (char *)se_hdr.e_txtstart);
+getbytes(se_hdr.e_fname, se_hdr.e_rodatoff, se_hdr.e_rodatlen, (char *)se_hdr.e_rodatstart);
+memset((char *)se_hdr.e_bssstart, 0,  se_hdr.e_bsslen);
+
+MAGIC_BREAK;
+// set_eflags((get_eflags() | EFL_RESV1) & ~EFL_AC);
+set_ss();
 
 
-
-
-
-
-
+lprintf("nop");
 
     while (1) {
         continue;
--- kern/memory/mem_mgmt.c
+++ kern/memory/mem_mgmt.c
@@ -2,6 +2,8 @@
  #include <malloc.h>
 #include "simics.h"
 #include "mem_internals.h"
+
+static KF *frame_base;
 /* Initialize the whole memory system */
 KF *mm_init() {
 
@@ -17,18 +19,18 @@ KF *mm_init() {
 	{
 		void *PTE = smemalign(4096, 1024*4);
 		
-		*(PD + i) = (uint32_t)PTE | 0x107;	
+		PD[i] = (uint32_t)PTE;	
 		// lprintf("the page table is in %p", PTE);
 		// lprintf("check we get the correct thign: %x", (unsigned int)*(PD + i));	
 	}
 
-	KF *frame_base = (KF*)smemalign(4096, 8 * 65536); // bunch of pointers that points to pages
+	frame_base = (KF*)smemalign(4096, 8 * 65536); // bunch of pointers that points to pages
 
 	for (i = 0; i < 65536; ++i)
 	{
-		(*(frame_base + i)).flag=0;
+		frame_base[i].flag=0;
 
-		(*(frame_base + i)).address = (void *)(0x0+i*4096);
+		frame_base[i].address = (void *)(0x0+i*4096);
 	}
 
 	lprintf("the address for frame is %p", frame_base);
@@ -39,12 +41,14 @@ KF *mm_init() {
 	int j;
 	for ( i = 0; i < 4; ++i)
 	{	
-		uint32_t PT = *(PD + i)& 0xFFFFF000;
-		lprintf("the pt is %u", (unsigned int)PT);
+		
+		uint32_t PT = PD[i];
+		PD[i] |= 0x107;
+		lprintf("the pt is %x", (unsigned int)PT);
 		for (j = 0; j < 1024; ++j)
 		{
-			uint32_t k = (uint32_t)((*(frame_base + 1024*i+j)).address);
-			(*(frame_base + 1024*i+j)).flag = 1;
+			uint32_t k = (uint32_t)(frame_base[1024*i+j].address);
+			frame_base[1024*i+j].flag = 1;
 			// lprintf("the page address is %x", (unsigned int)k);
 			*((uint32_t*)(PT) + j) =  k| 0x107;
 		}
@@ -71,10 +75,70 @@ uint32_t virtual2physical(uint32_t virtual_addr) {
 	return 0;
 }
 
-/* allocate memory and keep track the allocation */
-uint32_t *allocate(size_t size) {
-	return 0;
+/* Map this virtual address to a physical page */
+void allocate_page(uint32_t virtual_addr, size_t size) {
+	int i ;
+	lprintf("the address is %x", (unsigned int)virtual_addr);
+
+	uint32_t pd = virtual_addr >> 22;
+	lprintf("the pd is %x", (unsigned int)pd);
+	uint32_t pt = (virtual_addr & 0x3ff000)>>12;
+	lprintf("the pt is %x", (unsigned int)pt);
+
+	uint32_t offset = (virtual_addr & 0xfff)+ (uint32_t)size;
+	lprintf("the offset is %x", (unsigned int)offset);
+
+	uint32_t times = offset % 4096 == 0 ? offset/4096 : offset/4096+1;
+int j =0;
+	for (i = 0; i < 65536; ++i)
+	{
+		if (frame_base[i].flag == 0) {
+			lprintf("times to allocation a page: %u", (unsigned int)times);
+			times--;
+			frame_base[i].flag = 1;
+
+			uint32_t *PD = (uint32_t*)get_cr3();
+			uint32_t pde = PD[pd];
+			lprintf("page table entry: %x", (unsigned int)pde);
+
+			uint32_t *PT = (uint32_t *)(pde & 0xfffffffc);
+			lprintf("page table entry: %x", (unsigned int)PT);
+
+		 	PT[pt+j] = (uint32_t)frame_base[i].address|0x3;
+		 	lprintf("this is adjusted address %x  ",(unsigned int)frame_base[i].address);
+		 				pde |= 0x3;
+			PD[pd]=pde;
+			j++;
+		 	// break;
+		 	if (times == 0)
+		 	{
+		 		break;
+		 	}
+		 	
+		}
+	}
+
+
 }
+
+// void allocate_page(uint32_t virtual_addr, size_t size) {
+// 	int i ;
+// 	uint32_t pd = virtual_addr >> 22;
+// 	uint32_t pt = virtual_addr & 0x3ff000;
+// 	// uint32_t offset = virtual_addr & 0x111;
+// 	for (i = 0; i < 65536; ++i)
+// 	{
+// 		if (frame_base[i].flag == 0) {
+// 			frame_base[i].flag = 1;
+// 			uint32_t *page_table = (uint32_t*)get_cr3()+pd;
+// 		 	*(page_table + pt) = (uint32_t)frame_base[i].address|0x107;
+// 		 	//lprintf("so therefore %x, %d", (unsigned int)*(page_table + pt), i);
+// 		 	break;
+// 		}
+// 	}
+
+
+// }
 
 /* The reverse for allocate, free the memory out of the memory system */
 void freeff(uint32_t *addr) {
