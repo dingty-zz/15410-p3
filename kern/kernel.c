9c9dbdf7d429f1e9ce02c5edeeb37af9dd1f9480
tding <tding@andrew.cmu.edu>
2014-11-15 17:49:25 -0500

    Works good to me, passed make_crash but I don't know why, we should make sure

--- kern/kernel.c
+++ kern/kernel.c
@@ -94,7 +94,7 @@ int kernel_main(mbinfo_t *mbinfo, int argc, char **argv, char **envp)
     // process_create("yield_desc_mkrun", 1);
     // process_create("slaughter", 1);
     // process_create("actual_wait", 1);
-    // process_create("make_crash", 1);
+    // process_create("make_crash", 1);    
 
     // process_create("getpid_test1", 1);   // we run this thread
     // process_create("sleep_test1", 1);   // we run this thread
@@ -104,7 +104,7 @@ int kernel_main(mbinfo_t *mbinfo, int argc, char **argv, char **envp)
 
     // process_create("minclone_mem",1);
 
-    process_create("init",1);
+    // process_create("init",1);
     // process_create("ck1");
 
     // process_create("cho2",1);
@@ -113,7 +113,7 @@ int kernel_main(mbinfo_t *mbinfo, int argc, char **argv, char **envp)
     // For future use
 
 
-    // process_create("init", 1);   // we run this thread
+    process_create("init", 1);   // we run this thread
 
     while (1) continue;
 
