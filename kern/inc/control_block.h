798c4ecd108d1b96c28c3bbccd53152363d6f566
tding <tding@andrew.cmu.edu>
2014-11-15 03:01:44 -0500

    Add doc for control block

--- kern/inc/control_block.h
+++ kern/inc/control_block.h
@@ -18,16 +18,31 @@
 #include "locks/mutex_type.h"
 #include "mem_internals.h"
 
+// The thread is exited, set by vanish()
 #define THREAD_EXIT -2
+
+// Sets when the thread calls deschedule()
 #define THREAD_BLOCKED -1
+
+// The thread is running
 #define THREAD_RUNNING 0
+
+// The thread is runnable
 #define THREAD_RUNNABLE 1
-#define THREAD_WAITING 2     // only set when deschedule() is called
+
+// The thread is waiting for another thread to exit
+#define THREAD_WAITING 2 
+
+// The thread is sleeping by sleep()
 #define THREAD_SLEEPING 3
+
+// The thread is just created, has never run
 #define THREAD_INIT 4
+
+// The thread is calling readline and should block
 #define THREAD_READLINE 5
 
-
+// The process is in exit state, waiting for parent to reap it
 #define PROCESS_EXIT -2
 #define PROCESS_BLOCKED -1
 #define PROCESS_RUNNING 0
@@ -39,92 +54,114 @@
 typedef struct PCB_t
 {
     // Process id
-    int pid; 
-    // Can be the state listed above  
-    int state; 
+    int pid;
+    // Can be the state listed above
+    int state;
     // This is the return state for this process, set by set_status sys-call
-    int return_state;   
+    int return_state;
     // The parent that creates this process via fork
-    struct PCB_t* parent;      
+    struct PCB_t *parent;
 
-// All threads that this process has, including self thread  
-    list threads;      
+    // All threads that this process has, including self thread
+    list threads;
 
-    list children; // saves all forked child
-    node all_processes_node;
-    node peer_processes_node; 
-    uint32_t* PD;
+    // Saves all forked children
+    list children;
+
+    // The inner node that is used for storing peer-process-queue
+    node peer_processes_node;
+
+    // The page directory pointer for this process
+    uint32_t *PD;
+
+    // The number of live children that are created by this process via fork
     int children_count;
-    list va; //A list of va_info
 
-}PCB;
+    //A list of va_info
+    list va;
+
+} PCB;
 
 
 typedef struct s_info
 {
-    void* esp3;
-    void* eip;
-    void* arg;
-    ureg_t* newureg;
-    int installed_flag; 
+    void *esp3;
+    void *eip;
+    void *arg;
+    ureg_t *newureg;
+    int installed_flag;
     unsigned int eflags; //eflags when page fault happens;
 } swexninfo;
 
 typedef struct TCB_t
 {
+    // The current kernel stack pointer that is used for context
+    // switching
     uint32_t esp;
 
-    PCB *pcb; //process the thread belongs to
+    // Process the thread belongs to
+    PCB *pcb; 
 
+    // Thread id
     int tid;
+
+    // Thread state, can be several as listed above
     int state;
 
-    // For thread sleeping
+    // The thread sleeps at this time point
     unsigned int start_ticks;
+
+    // The number of ticks that this thread should sleep
     unsigned int duration;
 
-    void *stack_base;           // KERNEL stack base
-    unsigned int stack_size;  // 4096 (1 page) by default
-    ureg_t registers;  // USER stack pointer is in here!
+// Thread kernel stack base pointer
+    void *stack_base;   
 
+    // 4096 (1 page) by default        
+    unsigned int stack_size;  
+
+    // Registers that the thread is used when it's running in user space
+    ureg_t registers;  
+
+    // The inner node that belongs to the list of peer threads created by
+    // this process via threadfork
     node peer_threads_node;
+
+    // The inner node that belongs to either runnable queue or blocked queue
     node thread_list_node;
 
+    // The inner node that belongs to the wait queue that waits for a mutex
     node mutex_waiting_queue_node;
+
+    // The mutex that protects this tcb
     mutex_t tcb_mutex;
+
+    // The swexn handler information
     swexninfo swexn_info;
-    node readline_node;
-}TCB;
+
+} TCB;
 
 
-// This points the current running thread
+// This points the current running thread, and therefore the kernel
+// will know which thread is trapping into the kernel space
 TCB *current_thread;
 
 // Information about processes/threads in the kernel
+
 uint32_t next_tid;
 
 uint32_t next_pid;
 
-// Both THREAD_BLOCKED and THREAD_SLEEPING are put in this queue
+// Thread that has state THREAD_BLOCKED, THREAD_SLEEPING, 
+// THREAD_WAITING and THREAD_READLINE should go into this queue
 mutex_t blocked_queue_lock;
 list blocked_queue;
 
-// THREAD_RUNNABLE is put in this queue
+// Thread that has state THREAD_RUNNABLE or THREAD_INIT should go in this queue
 mutex_t runnable_queue_lock;
 list runnable_queue;
 
-
-// mutex_t readline_queue_lock;
-// mutex_t readline_work_lock;
-// list readline_queue;
-//-1 if no thread is reading
-// int next_read_thread = -1;
-
-
-// Not sure if this is useful
-mutex_t process_queue_lock;
-list process_queue;
-
+// The number of free physical 
 int free_frame_num;
 
 int total_num; //total number of chars in a line (to prevent deleting 410 shell phrases)
