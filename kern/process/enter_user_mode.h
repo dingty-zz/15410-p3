295d5972b340d7ff5a06e9c7004e42a969d105a3
tding <tding@andrew.cmu.edu>
2014-11-16 23:11:03 -0500

    Add comments in the process

--- kern/process/enter_user_mode.h
+++ kern/process/enter_user_mode.h
@@ -1,8 +1,8 @@
 /**
- * @file enter_user_mode.h
+ * @file enter_user_mode.S
  *
- * @brief This file defines two status of the spinlock and 
- *          several lock manipulation functions.
+ * @brief The file provide function that pops the initial registers from the
+ *        kernel stack for a thread and let it enter the user mode by iret
  *
  * @author Xianqi Zeng (xianqiz)
  * @author Tianyuan Ding (tding)
@@ -12,18 +12,21 @@
 #ifndef _ENTER_USER_MODE_H
 #define _ENTER_USER_MODE_H
 
+/** @brief Pop all registers from the current kernel stack and
+ *         let it enter the user mode by iret
+ */
 void enter_user_mode(uint32_t edi,
-                    uint32_t esi,
-                    uint32_t ebp,
-                    uint32_t ebx,
-                    uint32_t edx,
-                    uint32_t ecx,
-                    uint32_t eax,
-                    uint32_t eip,
-                    uint32_t cs,
-                    uint32_t eflags,
-                    uint32_t esp,
-                    uint32_t ss);
+                     uint32_t esi,
+                     uint32_t ebp,
+                     uint32_t ebx,
+                     uint32_t edx,
+                     uint32_t ecx,
+                     uint32_t eax,
+                     uint32_t eip,
+                     uint32_t cs,
+                     uint32_t eflags,
+                     uint32_t esp,
+                     uint32_t ss);
 
 
 #endif /* _ENTER_USER_MODE_H */
