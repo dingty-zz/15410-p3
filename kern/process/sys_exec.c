1b650c2518f5b6e136a3645291b59f4810198065
tding <tding@andrew.cmu.edu>
2014-11-16 23:22:17 -0500

    Add more commits

--- kern/process/sys_exec.c
+++ kern/process/sys_exec.c
@@ -1,3 +1,18 @@
+ /**
+ * @file sys_exec.h
+ *
+ * @brief This file contains the exec system call implementation
+          it first verifies the passed in pointers, if all valid,
+          it copies execname and argvec on to the current kernel
+          stack, and remap the virtual memory by changing the page
+          directory. After loading all the elf sections to the user
+          memory, it the copies the content stored in the kernel
+          stack to the user stack. It also pushes the stack high
+          pointer and stack low pointer as well
+ * @author Xianqi Zeng (xianqiz)
+ * @author Tianyuan Ding (tding)
+ *
+ */
 #include <syscall.h>
 #include "control_block.h"
 #include "datastructure/linked_list.h"
@@ -14,44 +29,41 @@
 #include "process.h"
 #include "memory/vm_routines.h"
 #include "thread/thread_basic.h"
+#include <x86/asm.h>                /* enable_interrupts() */
 
-
+#define USER_STACK_HIGH 0xffffffff
+#define USER_STACK_LOW 0xffffc000
+#define PARAMETERS_COUNT 5
 extern list process_queue;
 extern uint32_t next_pid;
 
 extern TCB *current_thread;
 
-/* Two things to do:
-1. add return -1, exec_non_exist
-2. don't increase tid
-*/
-/** @brief Determine if the given queue is empty
+
+/** @brief Execute a process on top of the calling process
  *
- *  If top == bottom, we know there are nothing in the queue.
- *
- *  @param q The pointer to the queue
- *  @return int 1 means not empty and 0 otherwise
+ *  @param execname the new process name
+ *  @param argvec argument-string vector
+ *  @return int -1 when failed, otherwise never returns
  **/
 int sys_exec(char *execname, char *argvec[])
 {
     if (!is_user_addr(execname) || !addr_has_mapping(execname)) return -1;
     if (!is_user_addr(argvec) || !addr_has_mapping(argvec)) return -1;
+    // Allocate the name pointer to store execname onto kernel stack
     char *name = (char *)malloc(strlen(execname) + 1);
     strncpy(name, execname,strlen(execname)+1);
-    // name = "swexn_uninstall_test";
-    lprintf("The execname is %s", name);
-    lprintf("char %s, argvec: %p", execname, argvec);
+
+    // Load the process
     simple_elf_t se_hdr;
     int result = elf_load_helper(&se_hdr, name);
-    lprintf("after elf loader");
+    // If invalid program, we return -1
     if (result == NOT_PRESENT || result == ELF_NOTELF)
     {
-        lprintf("program not present in exec");
         // error, the file doesn't exist
         return -1;
     }
 
-    lprintf("in exec, program found, start to load");
     // Count the number of arguments
     int argc = 0;
     while (argvec[argc] != 0)
@@ -81,26 +93,21 @@ int sys_exec(char *execname, char *argvec[])
     }
     argv[argc] = NULL;
 
-    int l = 0;
-    for (l = 0; l < argc; ++l)
-    {
-        lprintf("The is %s\n", argv[l]);
-    }
-
-
-    lprintf("The argc==%d", argc);
-
+    // Get current process pcb
     PCB *process = current_thread -> pcb;
 
     // Unmap current page directory and free all its address space
     process -> PD = init_pd();
 
+    disable_interrupts();
     current_thread -> registers.eip = program_loader(se_hdr, process);
-    // set up kernel stack pointer possibly bugs here
+    enable_interrupts();
+    
+    // set up kernel stack pointer 
     set_esp0((uint32_t)(current_thread -> stack_base + current_thread -> stack_size));
 
     // Copy the content to the new user stack
-    char *dest = (char *)0xffffffff;
+    char *dest = (char *)USER_STACK_HIGH;
     char *vector[argc + 1];
 
     for (k = 0; k < argc; k++)
@@ -111,22 +118,21 @@ int sys_exec(char *execname, char *argvec[])
     }
     vector[argc] = NULL;
 
-    dest -= sizeof(int) * 5;
-    memcpy(dest, vector, sizeof(int) * 5);
-    *((unsigned int *)(current_thread -> registers.esp)) = 0xffffc000;
-    current_thread -> registers.esp -= 4;
+    dest -= sizeof(int) * PARAMETERS_COUNT;
+    memcpy(dest, vector, sizeof(int) * PARAMETERS_COUNT);
+    *((unsigned int *)(current_thread -> registers.esp)) = USER_STACK_LOW;
+    current_thread -> registers.esp -= sizeof(int);
 
-    *(unsigned int *)current_thread -> registers.esp = 0xffffffff;
-    current_thread -> registers.esp -= 4;
+    *(unsigned int *)current_thread -> registers.esp = USER_STACK_HIGH;
+    current_thread -> registers.esp -= sizeof(int);
 
 
     *((unsigned int *)(current_thread -> registers.esp)) = (unsigned int)dest;
-    current_thread -> registers.esp -= 4;
+    current_thread -> registers.esp -= sizeof(int);
 
     *(int *)current_thread -> registers.esp = argc;
 
-    current_thread -> registers.esp -= 4;
-
+    current_thread -> registers.esp -= sizeof(int);
 
     enter_user_mode(current_thread -> registers.edi,     // let it run, enter ring 3!
                     current_thread -> registers.esi,
